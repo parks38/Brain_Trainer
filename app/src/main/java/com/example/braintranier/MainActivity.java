16e047a42fc2e1cf975dff9bd3842565913c8ee1
Sunny Park <Sunny@Sunnys-MacBook-Air.local>
2020-01-20 21:46:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/braintranier/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.braintranier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // update timer
+
+    public void timer (){
+
+    }
+
+    // update questions
+
+    //update score
+
+    // update choices
+
+    //update right or wrong text & hide
+
+    // when the game is over, show a button whether to replay or not
+}
